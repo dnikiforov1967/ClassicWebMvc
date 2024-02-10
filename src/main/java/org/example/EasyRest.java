4fa7b452099c765002f8854bd20e9ee10c31439a
Dmitry Nikiforov <dnikiforov@mail.ru>
2024-02-10 18:03:34 +0300

    Move all check specifications into interface

--- src/main/java/org/example/EasyRest.java
+++ src/main/java/org/example/EasyRest.java
@@ -1,12 +1,9 @@
 package org.example;
 
-
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@Validated
 public class EasyRest
         implements IEasyRest
 {
--- src/main/java/org/example/IEasyRest.java
+++ src/main/java/org/example/IEasyRest.java
@@ -3,7 +3,10 @@ package org.example;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.validation.annotation.Validated;
 
+
+@Validated
 public interface IEasyRest {
     //Liskov principle
     Integer easy(@Valid @NotNull(message = "WHat is it") Horse horse, @NotNull(message="I am stupid") @Min(value=2, message="Minimum 2") Integer paramA);
