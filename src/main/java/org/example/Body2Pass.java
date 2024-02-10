f2b11f3cebf2eccb3e322f5e69393047d4bedddf
Dmitry Nikiforov <dnikiforov@mail.ru>
2024-02-10 21:44:31 +0300

    Refactoring

--- src/main/java/org/example/Body2Pass.java
+++ src/main/java/org/example/Body2Pass.java
@@ -7,6 +7,5 @@ import lombok.Data;
 @Data
 public class Body2Pass {
 
-    @NotBlank
     private String name;
 }
--- src/main/java/org/example/IEasyRest.java
+++ src/main/java/org/example/IEasyRest.java
@@ -1,14 +1,10 @@
 package org.example;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.validation.annotation.Validated;
 
-
-@Validated
 public interface IEasyRest {
 
-    String postExample(@Valid Body2Pass body2Pass);
-    Integer getExample(@NotNull(message="I am stupid") @Min(value=2, message="Minimum 2") Integer param2Pass);
+    String postExample(Body2Pass body2Pass);
+    Integer getExample(Integer param2Pass);
 }
