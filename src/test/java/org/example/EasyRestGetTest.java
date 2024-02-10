1dab796a4cdcc1f77700ba86771cbf3d6a056c79
Dmitry Nikiforov <dnikiforov@mail.ru>
2024-02-10 23:07:15 +0300

    Refactoring

--- src/test/java/org/example/EasyRestGetTest.java
+++ src/test/java/org/example/EasyRestGetTest.java
@@ -7,7 +7,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 @WebMvcTest
@@ -17,22 +16,13 @@ public class EasyRestGetTest {
     private MockMvc mockMvc;
 
     @Test
-    void easyRestGetTestOk() throws Exception {
+    void easyRestGetTest() throws Exception {
         MvcResult mvcResult = mockMvc.perform(
                 get("/easy")
                         .queryParam("param", "3")
         ).andReturn();
         assertEquals(200, mvcResult.getResponse().getStatus());
         assertEquals("3", mvcResult.getResponse().getContentAsString());
-    }
-
-    @Test
-    void easyRestGetTestNoParam() throws Exception {
-        MvcResult mvcResult = mockMvc.perform(
-                get("/easy")
-        ).andReturn();
-        assertEquals(400, mvcResult.getResponse().getStatus());
-        assertEquals("{\"message\":\"Required request parameter 'param' for method parameter type Integer is not present\",\"errorCode\":-100}", mvcResult.getResponse().getContentAsString());
     }
 
 }
