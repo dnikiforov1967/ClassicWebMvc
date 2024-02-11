dd5ebee190a5d675f0ab6e3bdf493ac4f664a98e
Dmitry Nikiforov <dnikiforov@mail.ru>
2024-02-11 22:31:28 +0300

    Refactoring

--- src/main/java/org/example/EasyPostRest.java
+++ src/main/java/org/example/EasyPostRest.java
@@ -1,14 +1,16 @@
 package org.example;
 
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 
 @RestController
+@Validated
 public class EasyPostRest
         implements IEasyPostRest
 {
     @PostMapping("/easy")
-    public String postExample(@RequestBody Body2Pass body) {
+    public String postExample(@RequestBody(required = false) Body2Pass body) {
         return body.getName();
     }
 
--- src/main/java/org/example/IEasyPostRest.java
+++ src/main/java/org/example/IEasyPostRest.java
@@ -1,8 +1,9 @@
 package org.example;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 public interface IEasyPostRest {
 
-    String postExample(@Valid Body2Pass body);
+    String postExample(@Valid @NotNull(message="Missed body") Body2Pass body);
 }
